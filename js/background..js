06fceae0986444b590ea3ce601e3f546db606ebf
DDoEhub <rotlwnd@gmail.com>
2022-02-17 17:16:15 +0900

    complete todo js

--- js/background..js
+++ js/background..js
@@ -7,5 +7,6 @@ const chosenImage = images[Math.floor(Math.random() * images.length)];
 const randomImg = document.createElement("img");
 
 randomImg.src = `img/${chosenImage}`;
+randomImg.style.width = `100%`
 
 document.body.appendChild(randomImg);
--- js/todo.js
+++ js/todo.js
@@ -1,18 +1,60 @@
 const todoForm = document.querySelector("#todo-form");
 const todoInput = document.querySelector("#todo-form input");
-// const todoBtn = document.querySelector("#todo-form button");
 const todoListArea = document.querySelector("#todo-list")
 
-function inputTodoList(event){
-    event.preventDefault();
+const TODOS_KEY = "todos"
 
-    const todoList = document.createElement("li");
-    todoList.innerText = todoInput.value;
-    todoListArea.append(todoList);
+let toDos = [];
 
-
-    todoInput.value = "";
-
+function saveToDos() {
+    localStorage.setItem(TODOS_KEY, JSON.stringify(toDos))
 }
 
+function delList(event) {
+    const li = event.target.parentElement;
+
+    li.remove();
+    toDos = toDos.filter((todo) => todo.id !== parseInt(li.id));
+    saveToDos();
+}
+
+function paintList(newTodo) {
+    const li = document.createElement("li");
+    li.id = newTodo.id;
+    const todo = document.createElement("span");
+    todo.innerText = newTodo.text;
+    const del = document.createElement("button");
+    del.innerText = `delete`;
+    del.addEventListener("click", delList)
+
+    li.appendChild(todo);
+    li.appendChild(del);
+    todoListArea.appendChild(li);
+}
+
+function inputTodoList(event) {
+    event.preventDefault();
+
+    const newTodo = todoInput.value;
+    const newTodoObj = {
+        text: newTodo,
+        id: Date.now()
+    };
+
+    toDos.push(newTodoObj);
+    paintList(newTodoObj);
+    saveToDos();
+
+    todoInput.value = "";
+}
+
+
 todoForm.addEventListener("submit", inputTodoList);
+
+const savedToDos = localStorage.getItem(TODOS_KEY);
+const parseToDos = JSON.parse(localStorage.getItem(TODOS_KEY));
+
+if (savedToDos !== null) {
+    parseToDos.forEach(paintList);
+    toDos = parseToDos;
+};
